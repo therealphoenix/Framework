c8c7f6b68819e50ae33ae86ca5a22b9baae7fd8d
therealphoenix <fennya@gmail.com>
2017-02-28 19:12:24 +0200

    aferCode

--- src/main/java/com/klindziuk/microframework/CommandExecutor.java
+++ src/main/java/com/klindziuk/microframework/CommandExecutor.java
@@ -12,35 +12,30 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CommandExecutor {
+	private final String OPEN = "open";
+	private final String LINK_BY_HREF = "checkLinkPresentByHref";
+	private final String LINK_BY_NAME = "checkLinkPresentByName";
+	private final String PAGE_TITLE = "checkPageTitle";
+	private final String PAGE_CONTAINS = "checkPageContains";
 
 	private StringBuilder builder = new StringBuilder();
-	boolean testResult;
 	private FrameWorkCommands frame;
 	private String logPath;
+	private String line;
 	private long startTime;
+	private float fullTimeOfTests;
 	private int quantityOFTests;
 	private int passedCount;
 	private int failedCount;
-	private float fullTimeOfTests;
-
-	public void writeLog() {
-		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(logPath));
-			writer.write(printResult());
-			writer.close();
-		} catch (FileNotFoundException e) {
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
+	private boolean testResult;
+	
 	public void execute(String importFile) {
 		frame = new FrameWorkCommands();
 		try {
 			File file = new File(importFile);
 			Scanner scanner = new Scanner(file);
 			while (scanner.hasNextLine()) {
-				String line = scanner.nextLine();
+				line = scanner.nextLine();
 				List<String> listOfCommands = new ArrayList<>();
 				Matcher m = Pattern.compile("([^\"]\\S*|\".+?\")\\s*").matcher(line);
 				while (m.find()) {
@@ -50,43 +45,62 @@ public class CommandExecutor {
 				startTestTime();
 				switch (methodname) {
 
-				case "open": {
+				case OPEN: {
 					testResult = frame.open1(listOfCommands.get(1), listOfCommands.get(2));
 					break;
 				}
-				case "checkPageTitle": {
+				case PAGE_TITLE: {
 					testResult = frame.checkPageTitle(listOfCommands.get(1));
 					break;
 				}
-				case "checkPageContains": {
+				case PAGE_CONTAINS: {
 					testResult = frame.checkPageContains(listOfCommands.get(1));
 					break;
 				}
-				case "checkLinkPresentByHref": {
+				case LINK_BY_HREF: {
 					testResult = frame.checkLinkPresentByHref(listOfCommands.get(1));
 					break;
 				}
-				case "checkLinkPresentByName": {
+				case LINK_BY_NAME: {
 					testResult = frame.checkLinkPresentByName(listOfCommands.get(1));
 					break;
 				}
+				default: {
+					testResult = false;
+					System.out.println("Unfortunately we don't support test for \"" + methodname + "\".");
+				}
+                break;
+				
 				}
 				finishTestTime();
 				countTestResult();
-				String executionResult = "";
-				executionResult = testResult ? " + " : " ! ";
+				String executionResult  = testResult ? " + " : " ! ";
 				builder.append(executionResult + "[" + line + "]\n");
 			}
 			scanner.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} finally {
+		} catch (FileNotFoundException fnfe) {
+			System.out.println("1");
+			fnfe.printStackTrace();
+		} catch (IndexOutOfBoundsException iobe) {
+			System.out.println("Not enought arguments in command: "+ line + ". Test with this command failed." );
+			testResult = false;
+			finishTestTime();
+			countTestResult();
+			builder.append(" ! " + "[" + line + "]\n");
+		}
+		finally {
 			writeLog();
 		}
 	}
 
 	private void startTestTime() {
 		startTime = System.nanoTime();
+	}
+	
+	private void finishTestTime() {
+		long endTime = System.nanoTime();
+		long duration = (endTime - startTime) / 100000000;
+		fullTimeOfTests = fullTimeOfTests + duration;
 	}
 
 	private void countTestResult() {
@@ -97,19 +111,26 @@ public class CommandExecutor {
 			failedCount++;
 		}
 	}
-
-	private void finishTestTime() {
-		long endTime = System.nanoTime();
-		long duration = (endTime - startTime) / 100000000;
-		fullTimeOfTests = fullTimeOfTests + duration;
+	
+	public void writeLog() {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(logPath));
+			writer.write(printResult());
+			writer.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Unfortunately we can't open file at this path");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+	
 
 	private String printResult() {
 		builder.append(String.format("Total tests: %s  \n", quantityOFTests))
 				.append(String.format("Passed/Failed: %d/%d \n", passedCount, failedCount))
 				.append(String.format("Total time: %.3f \n", fullTimeOfTests))
 				.append(String.format("Average time: %.3f \n", fullTimeOfTests / quantityOFTests));
-		System.out.println(builder.toString()); // for testing
+		System.out.println(builder.toString()); // output for user
 		return builder.toString();
 	}
 
--- src/main/java/com/klindziuk/microframework/FrameWorkCommands.java
+++ src/main/java/com/klindziuk/microframework/FrameWorkCommands.java
@@ -1,14 +1,9 @@
 package com.klindziuk.microframework;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.*;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -20,80 +15,65 @@ public class FrameWorkCommands {
 
 	// JSoup only downloads html code that is present in that page.
 	// It does not download JavaScript-generated html, linked css or js or
-	// images.
+	// images.That's why our tests will run faster.
 	public boolean open1(String URL, String timeout) {
-
+		
 		int CONNECTION_TIMEOUT_MS = Integer.parseInt(timeout) * 1000;
 
 		try {
-			document = Jsoup.connect(URL).data("query", "Java").timeout(CONNECTION_TIMEOUT_MS).get();
-		} catch (IOException e) {
-
-			e.printStackTrace();
-		}
-		return true;
-	}
-
-	public boolean open(String URL, String timeout) {
-
-		// Timeout in millis.
-		int CONNECTION_TIMEOUT_MS = Integer.parseInt(timeout) * 1000;
-
-		try {
-
-			RequestConfig requestConfig = RequestConfig.custom().setConnectionRequestTimeout(CONNECTION_TIMEOUT_MS)
-					.setConnectTimeout(CONNECTION_TIMEOUT_MS).setSocketTimeout(CONNECTION_TIMEOUT_MS).build();
-
-			CloseableHttpClient httpClient = HttpClients.createDefault();
-			HttpGet request = new HttpGet(URL);
-			request.setConfig(requestConfig);
-			HttpResponse response = httpClient.execute(request);
-
-			// Get the response
-			BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-			StringBuilder builder = new StringBuilder();
-
-			String html = "";
-			while ((html = reader.readLine()) != null) {
-				builder.append(html);
+			Connection.Response response = Jsoup.connect(URL).timeout(CONNECTION_TIMEOUT_MS).execute();
+			if(200 == response.statusCode()) {
+			document = response.parse();
+			return true;
 			}
-			// html = builder.toString();
-			document = Jsoup.parse(builder.toString());
-
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 
-		} catch (UnsupportedOperationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (IllegalArgumentException ieax) {
+			System.out.println("Unfortunately we can't try open " + "\"" + URL + "\"");
+			return false;
 		}
-		return true;
+		return false;
 	}
 
 	public boolean checkLinkPresentByName(String name) {
-		Elements links = document.select("a[href]");
-		for (Element link : links) {
-			if (name.equals(link.text()))
-				return true;
+		if ((null == document) || (null == name)) {
+			return false;
+		} else {
+			Elements links = document.select("a[href]");
+			for (Element link : links) {
+				if (name.equals(link.text()))
+					return true;
+			}
 		}
 		return false;
 	}
 
 	public boolean checkLinkPresentByHref(String href) {
-		Elements links = document.select("a[href]");
-		for (Element link : links) {
-			if (href.equals(link.attr("href")))
-				return true;
+		if ((null == document) || (null == href)) {
+			return false;
+		} else {
+			Elements links = document.select("a[href]");
+			for (Element link : links) {
+				if (href.equals(link.attr("href")))
+					return true;
+			}
 		}
 		return false;
 	}
 
 	public boolean checkPageTitle(String text) {
-		return text.equals(document.title());
+		if ((null == document) || (null == text)) {
+			return false;
+		} else {
+			return text.equals(document.title());
+		}
 	}
 
 	public boolean checkPageContains(String text) {
-		return document.toString().contains(text);
+		if ((null == document) || (null == text)) {
+			return false;
+		} else {
+			return document.toString().contains(text);
+		}
 	}
 }
